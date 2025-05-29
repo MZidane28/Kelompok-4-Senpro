89c558371579aab0a1420eb1853398d197fe03ac
Wafi-Afdi <wafiafdi@gmail.com>
2025-05-29 18:43:13 +0700

    fix: CORS in production

--- backend/configs/corsConfig.js
+++ backend/configs/corsConfig.js
@@ -1,4 +1,4 @@
-let allowedOrigins = ['http://localhost:3000'];
+let allowedOrigins = ['http://localhost:3000', 'https://empati.biz.id'];
 
 const corsConfig = () => {
     if(process.env.environment == "production") {
